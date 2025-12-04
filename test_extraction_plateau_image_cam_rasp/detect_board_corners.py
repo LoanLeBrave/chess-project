67c7abdda7abc06b3eef5e39ae4386e3e4ed46cf
loan-benepro <loan.benepro@gmail.com>
2025-12-04 17:08:51 +0100

    Ajout extraction plateau: transformation perspective pour cropper l'image aux 4 ArUcos

--- test_extraction_plateau_image_cam_rasp/detect_board_corners.py
+++ test_extraction_plateau_image_cam_rasp/detect_board_corners.py
@@ -101,10 +101,10 @@ CALIBRATION_IDS = {
 #   - CAL_BL (bottom-left):  +x (droite), -y (haut)
 #   - CAL_BR (bottom-right): -x (gauche), -y (haut)
 OFFSETS = {
-    "CAL_TL": {"offset_x": 50, "offset_y": 50},      # +x vers droite, +y vers bas
-    "CAL_TR": {"offset_x": -50, "offset_y": 50},     # -x vers gauche, +y vers bas
-    "CAL_BL": {"offset_x": 50, "offset_y": -50},     # +x vers droite, -y vers haut
-    "CAL_BR": {"offset_x": -50, "offset_y": -50}     # -x vers gauche, -y vers haut
+    "CAL_TL": {"offset_x": 100, "offset_y": 100},      # +x vers droite, +y vers bas
+    "CAL_TR": {"offset_x": -100, "offset_y": 100},     # -x vers gauche, +y vers bas
+    "CAL_BL": {"offset_x": 100, "offset_y": -100},     # +x vers droite, -y vers haut
+    "CAL_BR": {"offset_x": -100, "offset_y": -100}     # -x vers gauche, -y vers haut
 }
 
 # Couleurs pour la visualisation (BGR pour OpenCV)
