69a6d54dbb4368d720bb2283b947b340a1fae95a
Destombes Yvi <99027395+yvi4410@users.noreply.github.com>
2025-12-05 10:06:24 +0100

    modif de l'ouverture de la pince

--- RobotPlay/chess_robot_final.py
+++ RobotPlay/chess_robot_final.py
@@ -21,9 +21,9 @@ from datetime import datetime
 ROBOT_IP = "192.168.0.11"
 VITESSE = 0.1
 ACCELERATION = 0.3
-GRIPPER_OUVERTURE = 50
+GRIPPER_OUVERTURE = 30
 DELTA_APPROCHE = 0.03  # 3cm au-dessus pour approche/remontée locale
-DELTA_TRANSIT = 0.40  # 20cm au-dessus pour le trajet entre cases
+DELTA_TRANSIT = 0.20  # 20cm au-dessus pour le trajet entre cases
 DELTA_RELACHE_BASE = 0.004  # 4mm minimum au-dessus pour poser
 
 # Hauteur de dépose par type de pièce (en mètres)
