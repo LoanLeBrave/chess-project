ecb180a7c53673f5b4a84eb9d77c2e06604699d7
loan-benepro <loan.benepro@gmail.com>
2025-12-05 10:04:57 +0100

    script de calibration 2

--- test_extraction_plateau_image_cam_rasp/detect_board_corners.py
+++ test_extraction_plateau_image_cam_rasp/detect_board_corners.py
@@ -101,10 +101,10 @@ CALIBRATION_IDS = {
 #   - CAL_BL (bottom-left):  +x (droite), -y (haut)
 #   - CAL_BR (bottom-right): -x (gauche), -y (haut)
 OFFSETS = {
-    "CAL_TL": {"offset_x": 78, "offset_y": 83},      # +x vers droite, +y vers bas
-    "CAL_TR": {"offset_x": -51, "offset_y": 89},     # -x vers gauche, +y vers bas
-    "CAL_BL": {"offset_x": 112, "offset_y": -40},     # +x vers droite, -y vers haut
-    "CAL_BR": {"offset_x": -102, "offset_y": -11}     # -x vers gauche, -y vers haut
+    "CAL_TL": {"offset_x": 0, "offset_y": 0},      # +x vers droite, +y vers bas
+    "CAL_TR": {"offset_x": 54, "offset_y": -86},     # -x vers gauche, +y vers bas
+    "CAL_BL": {"offset_x": -90, "offset_y": 112},     # +x vers droite, -y vers haut
+    "CAL_BR": {"offset_x": 53, "offset_y": 86}     # -x vers gauche, -y vers haut
 }
 
 # Couleurs pour la visualisation (BGR pour OpenCV)
